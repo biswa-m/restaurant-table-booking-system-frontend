0ca6085452151aeb6291985a3e86f86e556621a9
biswa-m <biswa12jit@gmail.com>
2019-01-22 13:27:12 +0530

    Add bootsrap package for styling

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -2,8 +2,41 @@
 	<div class="header">
 		<div class="header-container">
 			<slot name="navbar">
-				<router-link to="/">Home</router-link> |
-				<button id="login" @click="showLoginScreen">Login</button>
+				<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+					<a class="navbar-brand" href="#">TableBooking</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#headerNavbar" aria-controls="headerNavbar`" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="headerNavbar">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<div class="nav-link">
+									<router-link to="/">Home<span class="sr-only">(current)</span></router-link>
+								</div>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+								<div class="dropdown-menu" aria-labelledby="dropdown04">
+									<a class="dropdown-item" href="#">Action</a>
+									<a class="dropdown-item" href="#">Another action</a>
+									<a class="dropdown-item" href="#">Something else here</a>
+								</div>
+							</li>
+							<li>
+								<form class="form-inline my-2 my-md-0">
+									<input class="form-control" type="text" placeholder="Search">
+								</form>
+							</li>
+						</ul>
+						<div class="nav-link">
+							<router-link to="/restaurant/login">For Restauranteurs<span class="sr-only">(current)</span></router-link>
+						</div>
+						<div class="nav-link">
+							<button id="login" @click="showLoginScreen">Login</button>
+						</div>
+					</div>
+				</nav>
 			</slot>
 		</div>
 	</div>
