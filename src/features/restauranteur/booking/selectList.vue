ee20561dbd3bc0b1c4020f9705d0691edb1ed357
neo <biswa12jit@gmail.com>
2019-02-21 04:21:58 +0530

    Sort bookings for restauranteurs

--- src/features/restauranteur/booking/selectList.vue
+++ src/features/restauranteur/booking/selectList.vue
@@ -9,7 +9,23 @@
 			</div>
 			<hr>
 			<div class="row mr-1">
-				<div class="ml-auto">
+				<div class="ml-auto mr-1">
+					<b-dropdown variant="outline-info btn-sm" class="">
+						<template slot="button-content">
+							Sort By <b>{{sortKeys[sortby]}}</b>
+						</template>
+						<b-dropdown-item @click="sort('bookingFrom', sortby=='bookingFrom')">
+							Date<small v-show="sortby=='bookingFrom'"> ({{oppSortOrder()}})</small>
+						</b-dropdown-item>
+						<b-dropdown-item @click="sort('noOfPersons', sortby=='noOfPersons');">
+							Persons<small v-show="sortby=='noOfPersons'"> ({{oppSortOrder()}})</small>
+						</b-dropdown-item>
+						<b-dropdown-item @click="sort('bookingStatus', sortby=='bookingStatus');">
+							Status<small v-show="sortby=='bookingStatus'"> ({{oppSortOrder()}})</small>
+						</b-dropdown-item>
+					</b-dropdown>
+				</div>
+				<div class="ml-1">
 					<button class="btn btn-outline-info btn-sm"
 									@click="showFilter=!showFilter">
 						Filter Bookings
@@ -54,7 +70,7 @@
 					</div>
 				</b-col>
 				<b-col md=3 class="flex">
-					<span class="my-auto ml-0 mr-2">Per page</span>
+					<span class="my-auto ml-0 mr-2">Per Page</span>
 					<b-form-select :options="pageOptions" class="per-page" v-model="limit" />
 				</b-col>
 			</b-row>
@@ -76,7 +92,7 @@
 					</div>
 				</b-col>
 				<b-col md=3 class="flex">
-					<span class="my-auto ml-0 mr-2">Per page</span>
+					<span class="my-auto ml-0 mr-2">Per Page</span>
 					<b-form-select :options="pageOptions" class="per-page" v-model="limit" />
 				</b-col>
 			</b-row>
@@ -112,7 +128,15 @@
 				count: 0,
 				limit: 20,
 				skip: 0,
-				currentPage: 0
+				currentPage: 0,
+
+				sortby: 'bookingFrom',
+				sortOrder: 1,
+				sortKeys: {
+					'bookingFrom': 'Date',
+					'bookingStatus': 'Status',
+					'noOfPersons': 'Persons'
+				}
 			}
 		},
 
@@ -136,7 +160,7 @@
 				this.skip = 0;
 				this.currentPage = 1;
 				this.getBookings(this.params);
-			}
+			},
 		},
 
 		mounted() {
@@ -211,7 +235,21 @@
 				this.skip = (page - 1) * this.limit;
 				this.getBookings(this.params);
 				console.log('Page number: ', page, 'skip: ', this.skip);
-			}
+			},
+
+			sort(sortby, order) {
+				// if requesting to sort the same row, reverse the sorting order
+				this.sortOrder = order ? this.sortOrder * -1 : 1;
+				this.params.sortby=[[sortby, this.sortOrder]];
+
+				this.sortby = sortby;
+
+				this.getBookings(this.params);
+			},
+
+			oppSortOrder() {
+				return (this.sortOrder == -1) ? 'ascending' : 'descending'
+			},
 		}
 	}
 </script>
